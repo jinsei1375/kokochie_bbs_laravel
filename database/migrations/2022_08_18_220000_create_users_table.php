bf626191dbf8ba9caba2b2a5c927275f06115e5b
jinsei1375 <murakami@kokochie.jp>
2022-08-19 23:23:18 +0900

    first-commit

---
+++ database/migrations/2022_08_18_220000_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('your_id', 20)->uniqe();
+            $table->string('your_name', 20);
+            $table->string('password', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
